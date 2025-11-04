7e405f30f8963523e61fbbbafaa035eca6f80381
mexicode-dev <kamil.galazka@mexicode.dev>
2025-11-04 22:19:54 +0100

    Add Lesson_3 with

---
+++ Lesson_3/script.js
@@ -0,0 +1,12 @@
+// gsap.to('h1', { x: 800 });
+
+// // gsap.to('#crocodile', { x: 500, y: 100, scale: 1, rotation: 360, duration: 3 });
+
+// gsap.fromTo(
+//   '#crocodile',
+//   { x: 700, y: 400 },
+//   { x: 500, y: 100, scale: 1, rotation: 360, duration: 3 }
+// );
+
+gsap.to('#crocodile', { duration: 2, x: 600, repeat: 1, yoyo: true });
+gsap.to('#rabbit', { duration: 2, x: 600 });
